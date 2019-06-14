d051f6cf56c6d498e6278ed0885c488bf0e283c1
Johnson <JohnsonZ2@michigan.gov>
2019-06-14 12:02:59 -0400

    fixed sending functionality

--- src/main/java/gov/michigan/lara/bulkmail/controller/MailController.java
+++ src/main/java/gov/michigan/lara/bulkmail/controller/MailController.java
@@ -1,27 +1,57 @@
 package gov.michigan.lara.bulkmail.controller;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import gov.michigan.lara.bulkmail.domain.EmailObject;
 import gov.michigan.lara.bulkmail.service.MailService;
 import gov.michigan.lara.bulkmail.util.CsvUtils;
 
 @Controller
+@ControllerAdvice
 public class MailController {
+
+    int size = 0;
+    int count = 0;
+    Double percent = 0.0;
 
     private static final Logger log = LoggerFactory.getLogger(MailController.class);
     public MailService mailService = new MailService();
+
+    @ModelAttribute
+    @GetMapping(value = "/")
+    public ModelAndView test() {
+        ModelAndView mav = new ModelAndView("index");
+        this.size = 0;
+        this.count = 0;
+        mav.addObject("count", count);
+        mav.addObject("size", size);
+        return mav;
+    }
 
     @ResponseBody
     @PostMapping(value = "/check")
@@ -33,18 +63,79 @@ public class MailController {
         for (EmailObject e : emails) {
             log.info(e.getEmail());
         }
-        return emails.size();
+        this.size = emails.size();
+        return this.size;
     }
 
     @ResponseBody
     @PostMapping(value = "/load")
     public void load(@RequestParam("file") MultipartFile file, @RequestParam("from") String from,
-            @RequestParam("subject") String subject, @RequestParam("body") String body) throws IOException, InterruptedException, ExecutionException {
+            @RequestParam("subject") String subject, @RequestParam("body") String body, HttpServletRequest request)
+            throws IOException, InterruptedException, ExecutionException {
 
         List<EmailObject> emails = new ArrayList<EmailObject>();
         emails.addAll(CsvUtils.read(EmailObject.class, file.getInputStream()));
         log.info("sending emails for " + emails.size() + " addresses");
-        mailService.sendEmail(emails, from, subject, body);
+
+        this.size = emails.size();
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "coreosmtp.state.mi.us");
+        props.put("mail.debug", "false");
+        props.put("mail.smtp.auth", "false");
+        props.put("mail.smtp.ssl.trust", "coreosmtp.state.mi.us");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getInstance(props);
+        Message message = new MimeMessage(session);
+        try {
+            message.setFrom(new InternetAddress(from));
+            message.setSubject(subject);
+            message.setContent(body, "text/html");
+        } catch (MessagingException e) {
+            log.error("error creating message: " + e.getMessage());
+        }
+
+        List<String> fails = new ArrayList<String>();
+        request.getSession().setAttribute("fails", fails);
+
+        Future<Integer> future = null;
+        for (EmailObject email : emails) {
+            try {
+                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email.getEmail()));
+                future = mailService.actualSend(message, this.count, request);
+                this.count = future.get();
+            } catch (MessagingException ex) {
+                this.count++;
+                log.error("error sending message to '" + email.getEmail() + "' - " + ex.getMessage());
+            }
+            Double percent = new Double(this.count) / new Double(this.size) * 100;
+            this.percent = percent;
+            log.info("Total addresses processed: " + this.count + "/" + this.size + " = " + percent + "%");
+        }
+
+        if (count == this.size) {
+            log.info("mail process completed");
+            this.count = 0;
+            this.size = 0;
+            this.percent = 0.0;
+        }
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/getProgress")
+    public String getProgress() {
+        DecimalFormat dec = new DecimalFormat("#0");
+        Double percent = new Double(this.count) / new Double(this.size) * 100;
+        return dec.format(percent);
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/getFails")
+    public List<String> getProgress(HttpServletRequest request) {
+        @SuppressWarnings(value = "unchecked")
+        List<String> fails = (List<String>) request.getSession().getAttribute("fails");
+        return fails;
     }
 
 }
--- src/main/java/gov/michigan/lara/bulkmail/service/MailService.java
+++ src/main/java/gov/michigan/lara/bulkmail/service/MailService.java
@@ -1,16 +1,12 @@
 package gov.michigan.lara.bulkmail.service;
 
 import java.util.List;
-import java.util.Properties;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,66 +14,30 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 
-import gov.michigan.lara.bulkmail.domain.EmailObject;
-
 @Service
 public class MailService {
 
+    int size = 0;
+    int count = 0;
+
     private static final Logger log = LoggerFactory.getLogger(MailService.class);
 
-    public void sendEmail(List<EmailObject> emails, String from, String subject, String body) throws InterruptedException, ExecutionException {
-        log.info("Invoking an asynchronous method. " + Thread.currentThread().getName());
-
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "coreosmtp.state.mi.us");
-        props.put("mail.debug", "false");
-        props.put("mail.smtp.auth", "false");
-        props.put("mail.smtp.ssl.trust", "coreosmtp.state.mi.us");
-        props.put("mail.smtp.starttls.enable", "true");
-
-        Session session = Session.getInstance(props);
-
-        Message message = new MimeMessage(session);
-        try {
-            message.setFrom(new InternetAddress(from));
-            message.setSubject(subject);
-            message.setContent(body, "text/html");
-        } catch (MessagingException e) {
-            log.error("error creating message: " + e.getMessage());
-        }
-
-        Integer count = 0;
-        Future<Integer> future = null;
-        for (EmailObject email : emails) {
-            try {
-                Thread.sleep(3000);
-                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email.getEmail()));
-                future = actualSend(message, count);
-                count = future.get();
-            } catch (MessagingException ex) {
-                count++;
-                log.error("error sending message to '" + email.getEmail() + "' - " + ex.getMessage());
-            }
-            Double percent = new Double(count) / new Double(emails.size()) * 100;
-            log.info("Total addresses processed: " + count + "/" + emails.size() + " = " + percent + "%");
-        }
-
-        if (count == emails.size()) {
-            log.info("process completed");
-        }
-    }
-
     @Async
-    public Future<Integer> actualSend(Message message, Integer n) throws MessagingException {
+    public Future<Integer> actualSend(Message message, Integer n, HttpServletRequest request)
+            throws MessagingException {
         n++;
+        String address = message.getAllRecipients()[0].toString();
         try {
-            Thread.sleep(2000);
             Transport.send(message);
-            log.info("message sent to '" + message.getAllRecipients()[0] + "'");
-        } catch (InterruptedException e) {
-            log.error("process interrupted '" + message.getAllRecipients()[0] + "' - " + e.getMessage());
+            log.info("message sent to '" + address + "'");
         } catch (MessagingException e) {
-            log.error("error sending message to '" + message.getAllRecipients()[0] + "' - " + e.getMessage());
+            log.error("error sending message to '" + address + "' - " + e.getMessage());
+            @SuppressWarnings(value = "unchecked")
+            List<String> fails = (List<String>) request.getSession().getAttribute("fails");
+            fails.add(address);
+            request.getSession().setAttribute("fails", fails);
+        } finally {
+            
         }
         return new AsyncResult<Integer>(n);
     }
